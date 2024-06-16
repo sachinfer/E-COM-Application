48f066bde078714ba3e84658e146e723df4f6178
Rashmika <rashmika@atera.io>
2024-06-16 12:05:44 +0530

    Login UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/login';
 
 function App() {
   return (
     <div className="App">
-      Hello
-      This is new change
+      <Login/>
     </div>
   );
 }
