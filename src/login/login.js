4fe3caaaa7bff6ce49a2fc3a13eb1ca0d2bb2ce8
Rashmika <rashmika@atera.io>
2024-06-21 12:53:52 +0530

    login with google

--- src/login/login.js
+++ src/login/login.js
@@ -11,8 +11,13 @@ import {
 from 'mdb-react-ui-kit';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from 'react';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 function Login() {
+  
+  const provider = new GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
   const [email,setEmail] = useState();
   const [password,setPassword] =useState();
@@ -36,6 +41,27 @@ function Login() {
 
   function signupWithGoogle(){
     console.log("Sign up with Google")
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log("error", errorMessage)
+    // ...
+  });
   }
   
     return (
